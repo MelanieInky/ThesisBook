1fe2acaa74d0e910c69d60fa4633843f97d2d602
MelanieInky <melanie.fournier234@gmail.com>
2023-05-02 05:40:07 +0200

    more more writing

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from plotnine import *
 
 
 #########HYPERPARAMETERS
-slowpoke=1000
-slowerpoke = 1*slowpoke
+slowpoke=20000
+slowerpoke = 10*slowpoke
 sahnic=0.25
 
 gamma = 0.1
 exploration_rate = 0.1
 episode_length = 20
 learning_rate = 1e-8
-learning_mult = np.array([slowpoke,sahnic,slowerpoke,sahnic,slowpoke,slowpoke])
-initial_theta = np.array([0,0,0,0,0.3,2])
-n_episodes = 500000 #Number of episodes to train for
+learning_mult = np.array([slowpoke,sahnic,slowerpoke,sahnic,slowpoke,slowerpoke])
+initial_theta = np.array([0.,0.,0.,0.,0.,0.])
+n_episodes = 50000   #Number of episodes to train for
 #############
 
 
@@ -30,6 +30,10 @@ problem = TestProblem(gamma=gamma,
 
 # And a set theta parameters to start with
 problem.pol.set_theta(theta=initial_theta)
+
+
+######### Everything is set
+### Run from here to continue training
 
 # Prepare everything needed to log our training
 date = datetime.now().strftime('%d%b-%Hh%Mm%Ss')
@@ -45,7 +49,8 @@ with open(config_log_file, 'a') as f:
     f.write('Exploration rate/Std_dev: %s\n' % exploration_rate)
     f.write('Episode length: %s\n' % episode_length)
     f.write('Initial theta: ' + np.array2string(initial_theta, separator=',') + '\n')
-    f.write('Learning multiplier: ' + np.array2string(learning_mult, separator=','))
+    f.write('Learning multiplier: ' + np.array2string(learning_mult, separator=',') + '\n')
+    f.write('Number of episodes: '+  str(n_episodes))
 
 
 fileName = 'Log/' + date + '/log.csv'
@@ -70,7 +75,7 @@ df3 = df.select('ep_number', 'avg_ep_reward').with_columns(pl.col('avg_ep_reward
 p = (ggplot(df2, aes('episode_no_1000','value'))
 + geom_line()
 + facet_wrap('~variable', scales='free_y')
-+ theme_bw()
++ theme_seaborn()
 + theme(panel_spacing_x= 0.7)
 + xlab('Episode no. / 1000')
 + ylab('Theta')
@@ -81,7 +86,7 @@ p.draw()
 
 p2 = (ggplot(df3, aes('ep_number','rolling reward'))
 + geom_line()
-+ theme_bw()
++ theme_seaborn()
 + xlab('Episode number'))
 
 p2.draw()
